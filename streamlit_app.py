728463d80348efe7de120f5b63d7752b6bbb0286
Christian Heins <129910735+christianheins@users.noreply.github.com>
2023-07-27 18:22:33 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,10 @@
 import streamlit as st
 
 st.title('Proplist, Property Listings')
+st.subheader('This URL is an old one')
 
 url = "https://proplist.streamlit.app"
-link_text = "Click here to visit Proplist, Property Listings."
+link_text = "Click here to visit Proplist, Property Listings. under proplist.streamlit.app"
 
 # Create the clickable URL using Markdown
 st.markdown(f"[{link_text}]({url})")
