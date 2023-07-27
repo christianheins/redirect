bdf3a0ad9fe039f885f141e157d30f2c4b55f1e8
Christian Heins <129910735+christianheins@users.noreply.github.com>
2023-07-27 15:13:57 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ st.title('Proplist, Property Listings')
 
 st.header('Find this website now on:')
 url = "proplist.streamlit.app"
-link_text = "Click here to visit Proplist, Property Listings.
+link_text = "Click here to visit Proplist, Property Listings."
 
 # Create the clickable URL using Markdown
 st.markdown(f"[{link_text}]({url})")
